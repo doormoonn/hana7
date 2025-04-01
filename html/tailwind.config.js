7838c7ac0ef965da461512c37cb8f9051da8ad8a
moon <robort123@naver.com>
2025-04-01 10:19:03 +0900

    html first commit

---
+++ html/tailwind.config.js
@@ -0,0 +1,12 @@
+tailwind.config ={
+    theme: {
+        extend: {
+            colors: {
+                'cliford':"#da373d",
+                'danger':"#ef0000",
+                'primary': "#3490dc",
+                'carrot': "#e67e22",
+            }
+            }
+        }
+}
